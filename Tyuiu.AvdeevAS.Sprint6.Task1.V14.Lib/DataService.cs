6fe3cb68697d8a5e2427182737dc9237de3ad173
Tyuiu.AvdeevAS <Tyuiu.AvdeevAS@outlook.com>
2024-12-12 19:29:06 +0500

    Task1

---
+++ Tyuiu.AvdeevAS.Sprint6.Task1.V14.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.AvdeevAS.Sprint6.Task1.V14.Lib
+{
+    public class DataService : ISprint6Task1V14
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            if (startValue > stopValue)
+            {
+                throw new ArgumentException("Начальное значение не может быть больше конечного.");
+            }
+
+            List<double> results = new List<double>();
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                double denominator = Math.Cos(x) + x;
+                double value = denominator == 0 ? 0 : Math.Round(((2 * x + 6) / denominator) - 3, 2, MidpointRounding.AwayFromZero);
+                results.Add(value);
+            }
+
+            return results.ToArray();
+        }
+    }
+}
---
+++ Tyuiu.AvdeevAS.Sprint6.Task1.V14.Test/DataServiceTest.cs
@@ -0,0 +1,36 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.AvdeevAS.Sprint6.Task1.V14.Lib;
+
+namespace Tyuiu.AvdeevAS.Sprint6.Task1.V14.Tests
+{
+    [TestClass]
+    public class DataServiceTests
+    {
+        [TestMethod]
+        public void GetMassFunction_ValidInput_ReturnsCorrectResults()
+        {
+            // Arrange
+            var service = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double[] expected = { /* expected values */ };
+
+            // Act
+            double[] actual = service.GetMassFunction(startValue, stopValue);
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetMassFunction_InvalidRange_ThrowsArgumentException()
+        {
+            // Arrange
+            var service = new DataService();
+
+            // Act
+            service.GetMassFunction(5, -5);
+        }
+    }
+}
